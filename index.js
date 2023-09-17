ff585a3667bc25a1cbd4f3f99364b8e453ad6658
Tenzin Dorjee <tenzindorjee95@yahoo.com>
2023-09-17 14:00:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+
+  ]
+  function superbowlWin(record) {
+    const winningGame = record.find(function(game) {
+      return game.result === "W";
+    });
+    
+    if (winningGame) {
+      return winningGame.year;
+    } else {
+      return undefined;
+    }
+  }
+  
